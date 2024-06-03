7f30c0dc45635ad0152229840c09b1b34e8bd454
SamB-C <samson_bc@icloud.com>
2024-06-03 16:04:51 +0100

    Add example API call

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
+import requests
+POKEAPI_BASE_URL = "https://pokeapi.co/api/v2/"
+
 if __name__ == '__main__':
-    print("Hello World")
+    ditto_info = requests.get(POKEAPI_BASE_URL + "pokemon/ditto").json()
+    print(ditto_info)
