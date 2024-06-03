e2a7cad9452f48eb2fc7325b31b4b06190fa2529
Olly Harding <oliverdhardin@gmail.com>
2024-06-03 17:16:37 +0100

    Create Test.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import requests
+import json
+
 POKEAPI_BASE_URL = "https://pokeapi.co/api/v2/"
 
 if __name__ == '__main__':
-    ditto_info = requests.get(POKEAPI_BASE_URL + "pokemon/ditto").json()
-    print(ditto_info)
+    pokemon = input("Please enter the name of a pokemon or quit\n").lower()
+    response_API = requests.get(POKEAPI_BASE_URL + "pokemon/" + pokemon)
+    pokemon_info = response_API.text
+    parse_json = json.loads(pokemon_info)
+    querey = parse_json["abilities"]
+    print(querey)
