4d6662ce866672bf7d808d2ede3848b2fa5ec532
Olly Harding <oliverdhardin@gmail.com>
2024-06-03 17:18:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 POKEAPI_BASE_URL = "https://pokeapi.co/api/v2/"
 
 if __name__ == '__main__':
-    pokemon = input("Please enter the name of a pokemon or quit\n").lower()
+    pokemon = input("Please enter the name of a pokemon").lower()
     response_API = requests.get(POKEAPI_BASE_URL + "pokemon/" + pokemon)
     pokemon_info = response_API.text
     parse_json = json.loads(pokemon_info)
